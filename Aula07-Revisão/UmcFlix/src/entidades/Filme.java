eb30195754eedc94f536387d0613ce1ebb6b6578
VilchesBreno <155098846+brenovilches@users.noreply.github.com>
2025-09-16 10:33:01 -0300

    Aula-07

---
+++ Aula07-Revisão/UmcFlix/src/entidades/Filme.java
@@ -0,0 +1,24 @@
+package entidades;
+
+public class Filme extends Catalogo{
+    private int duracao;
+
+    public Filme(String nomeFilme, String protagonista, Genero genero, String diretor, int classificacao, int duracao) {
+        super(nomeFilme, protagonista, genero, diretor, classificacao);
+        this.duracao = duracao;
+    }
+    public int getDuracao() {
+        return duracao;
+    }
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    @Override
+    public String toString() {
+        return
+                super.toString() +
+                "Duração: " + duracao;
+
+    }
+}
